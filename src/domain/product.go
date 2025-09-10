1f49c8899dae2984f4d0e613282b86f44b738c4d
Rodrigo Goncalves <rodrigo.g.459@gmail.com>
2025-09-09 22:35:13 -0300

    feat: Adds product entity to domain layer

---
+++ src/domain/product.go
@@ -0,0 +1,13 @@
+package domain
+
+import "time"
+
+type Product struct {
+	ID          string    `json:"id" db:"id"`
+	Name        string    `json:"name" db:"name"`
+	Description string    `json:"description" db:"description"`
+	Price       float64   `json:"price" db:"price"`
+	Stock       int       `json:"stock" db:"stock"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
+}
